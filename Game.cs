b5aaa0670928f3b539b1f1c338b806adabca1c68
EdwardGreenway <edwardmacpherson1@gmail.com>
2025-04-20 16:07:54 +0000

    Add files via upload

--- Game.cs
+++ Game.cs
@@ -1,25 +1,75 @@
-﻿using System;
-using System.Media;
+﻿using MapTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace DungeonExplorer
 {
     internal class Game
     {
-        private Player player;
-        private Room currentRoom;
-
-        public Game()
+        public static bool Playing { get; set; }
+        public static Player Player {  get; set; }
+        public static Inventory Inventory { get; set; }                          
+        public static GameMap Map { get; set; }
+        static Game()
         {
-            // Initialize the game with one room and one player
+            Playing = true;
 
+            //Player (Name, Health, Health Limit, Inventory
+            Player = new Player("", 40, 100, null);
+
+            //Inventory (WeaponInvSize, PotionInvSize, Player
+            Inventory = new Inventory(3, 5, Player);
+            Player.Inventory = Inventory;
+
+            //GameMap (MapSizeY, MapSizeX)
+            Map = new GameMap(6,6, Player, Inventory);
+
+            Player.GetName();
         }
-        public void Start()
+        public static void Start()
         {
-            // Change the playing logic into true and populate the while loop
-            bool playing = false;
-            while (playing)
+            while (Playing == true && Player.Alive == true)
             {
-                // Code your playing logic here
+                NextMove();
+            }
+        }
+
+        public static void NextMove() 
+        {
+            Map.EnterRoom();
+
+            bool choosing = true;
+            while (choosing == true)
+            {
+                Console.WriteLine("Which action will you take next?\n- Move Rooms\n- Open Inventory\n- Check Status\n");
+
+                string choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "move rooms":
+                        choosing = false;
+                        Map.CheckExits();
+                        break;
+
+                    case "open inventory":
+                        choosing = false;
+                        Inventory.EngageInventory();
+                        break;
+
+                    case "check status":
+                        choosing = false;
+                        Player.ShowPlayerStatus();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input, try again...\n");
+                        break;
+                }
             }
         }
     }
